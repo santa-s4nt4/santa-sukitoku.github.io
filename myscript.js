2c46fe1fdbecbbf5412b5f30973d4e4823797470
santa-sukitoku <santanaruse@gmail.com>
2019-04-18 01:32:48 +0900

    I add a js file.

---
+++ myscript.js
@@ -0,0 +1,25 @@
+var r;
+
+function setup() {
+        createCanvas(windowWidth, windowHeight);
+        background(255);
+        rectMode(CENTER);
+        frameRate(random(6, 10));
+}
+
+function draw() {
+        r = random(25, 500);
+        for (var i = 0; i < 1; ++i) {
+                noFill();
+                stroke(random(100, 200));
+                ellipse(random(width), random(height), r, r);
+                fadeOut();
+        }
+}
+
+function fadeOut() {
+        noStroke();
+        fill(255, 120);
+        rectMode(CORNER);
+        rect(0, 0, width, height);
+}
