e7b55310030f1c0463bd94d6b1d3b143e708804c
santa-sukitoku <santanaruse@gmail.com>
2019-04-18 01:26:33 +0900

    add a js file

--- vedajs/lib/gif-loader.d.ts
+++
@@ -1,7 +0,0 @@
-import * as THREE from 'three';
-export default class GifLoader {
-    private cache;
-    update(): void;
-    load(name: string, url: string): THREE.Texture;
-    unload(url: string): void;
-}
