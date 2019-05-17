51a41f916089b5b6352486c77c0f07461e74c6f2
santa-sukitoku <santanaruse@gmail.com>
2019-05-18 02:19:49 +0900

    finish!

--- myscript.js
+++ myscript.js
@@ -3,13 +3,16 @@ function setup() {
 }
 
 function draw() {
-        var s = 400;
+        var s = windowWidth / 5;
+        var t = windowWidth / 20;
         background(0);
         rotateX(frameCount * 0.01);
         rotateY(frameCount * 0.01);
         noFill();
         stroke(255);
         sphere(s);
+        rotateZ(frameCount * 0.01);
+        torus(t, t);
 }
 
 function windowResized() {
