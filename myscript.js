f61d2ddde04916d9c2ebb6ef551dca67e8f4ba4b
santa-sukitoku <santanaruse@gmail.com>
2019-05-18 02:33:49 +0900

    fix

--- myscript.js
+++ myscript.js
@@ -3,8 +3,13 @@ function setup() {
 }
 
 function draw() {
-        var s = windowWidth / 5;
-        var t = windowWidth / 20;
+        if (widnowsWidht > 1300) {
+                var s = windowWidth / 5;
+                var t = windowWidth / 20;
+        } if (windowWidth <= 1300) {
+                var s = windowHeight / 5;
+                var t = windowHeight / 20;
+        }
         background(0);
         rotateX(frameCount * 0.01);
         rotateY(frameCount * 0.01);
