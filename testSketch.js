78c91c7d5fdba298b69600a73fd37794f1df3a3e
santa-sukitoku <santanaruse@gmail.com>
2019-11-22 02:19:06 +0900

    fftを色に変換

--- testSketch.js
+++ testSketch.js
@@ -22,11 +22,16 @@ function draw() {
     //strokeWeight(0);
     noStroke();
 
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i < spc.length; i++) {
         push();
         translate(width / 2, height / 2);
         rotate(slice * i);
+        let hue = map(i, 0, spc.length - 1, 0, 255);
+        let brightness = spc[i / 2];
+        stroke(hue, 255, brightness * 2);
+        fill(hue, 255, brightness);
         let d = map(spc[i], 0, 255, 0, height - (100 + r)) + r;
+        console.log(d);
         arc(0, 0, d, d, 0, slice, PIE);
         pop();
     }
