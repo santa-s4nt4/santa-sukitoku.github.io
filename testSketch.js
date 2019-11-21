d80c3f1c07f4f195b3a96630e79e53ce71fdfc23
santa-sukitoku <santanaruse@gmail.com>
2019-11-22 03:11:03 +0900

    FFTを色に変換するコードのパラメータ調整．

--- testSketch.js
+++ testSketch.js
@@ -19,22 +19,22 @@ function draw() {
     let spc = fft.analyze();
     let slice = TAU / num;
 
-    //strokeWeight(0);
-    noStroke();
+    strokeWeight(0);
 
     for (let i = 0; i < spc.length; i++) {
         push();
         translate(width / 2, height / 2);
         rotate(slice * i);
-        let hue = map(i, 0, spc.length - 1, 0, 255);
-        let brightness = spc[i / 2];
-        stroke(hue, 255, brightness * 2);
-        fill(hue, 255, brightness);
-        let d = map(spc[i], 0, 255, 0, height - (100 + r)) + r;
+        let hue = map(i / 2, 10, spc.length - 10, 0, 500);
+        let brightness = spc[i];
+        noStroke();
+        stroke(hue * 4, 255, brightness * 20);
+        fill(hue * 4, 255, brightness * 20);
+        let d = map(spc[i], 0, 80, 0, height - (100 + r)) + r;
         arc(0, 0, d, d, 0, slice, PIE);
         pop();
     }
 
-    fill(255);
+    noStroke();
     ellipse((width / 2), (height / 2), r);
 }
