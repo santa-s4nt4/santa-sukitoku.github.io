434577ea017e2ad8d4e8cc25723459dc29775ca5
santa-sukitoku <santanaruse@gmail.com>
2019-12-31 21:34:59 +0900

    github pagesにデプロイする準備

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 
 export default {
   router: {
-    base: '/<repository-name>/'
+    base: '/santa-sukitoku.github.io/'
   },
 
   mode: 'universal',
@@ -16,7 +16,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/santa-sukitoku.github.io/favicon.ico' }
     ],
     script: [
       { src: 'https://use.typekit.net/krb5kuj.js' }
