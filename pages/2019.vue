9dd58901d60f68a194b041484ef3c302b311266d
santa-sukitoku <santanaruse@gmail.com>
2020-03-24 17:37:51 +0900

    feat: 2019年ページを追加

---
+++ pages/2019.vue
@@ -0,0 +1,101 @@
+<template>
+<section class="container">
+  <body class="all">
+    <router-link to="/works/191231">
+      <div class="works">
+        <img
+          src="https://lh3.googleusercontent.com/iygRRR1CANo4x4zvkI95Fpw9hm1aK4eWcwqGWqpwOygVEIPTuC8d5JnWAGNtOVz7bmAou58BCAvY3cM6qazja2fRhClxTwYgGYYIJ591bqpDufkL3Jbxfj8MT9e2i1xLQjuOssSvR6Q=w2400"
+          width="100%"
+        />
+        <div class="mask">
+          <div class="caption">
+            <p>best photographs of 2019</p>
+          </div>
+        </div>
+      </div>
+    </router-link>
+
+    <router-link to="/works/191024">
+      <div class="works">
+        <img
+          src="https://lh3.googleusercontent.com/aUlxAwRBNudTcZf6DaE91xud3rTvao-gitIkfzgD_nr0Hg8v0q_3jyDTaR8aY-k-pEL_AhqHfGGRrbfO__e5AZFRUu-CImashxVX8JE7_1RH6IYaUXMy0AvQwTNjgrYXsmLwrjswlQw=w2400"
+          width="100%"
+        />
+        <div class="mask">
+          <div class="caption">
+            <p>visual system for SFC Creative Week</p>
+          </div>
+        </div>
+      </div>
+    </router-link>
+
+    <router-link to="/works/190726">
+      <div class="works">
+        <img
+          src="https://lh3.googleusercontent.com/VEMrUc_0_E-ec2byFk-MoCpHN4Zhcv825m7fx-mDs0k0tKrzOmURegZ49jfOchBb5QqNqEaYVzh_lV1h-uJORy-Fr-RYmbrQaD44kDd6kXyBwxVDUCDEaBpujp5XiZ45ycXv2cX5Dgw=w2400"
+          width="100%"
+        />
+        <div class="mask">
+          <div class="caption">
+            <p>
+              Computer Music1 final work
+              <br />"I & Forest"
+            </p>
+          </div>
+        </div>
+      </div>
+    </router-link>
+
+    <router-link to="/works/190717">
+      <div class="works">
+        <img
+          src="https://lh3.googleusercontent.com/z6cH0zUkyLu_hVsdjqCNNQoi64WmaCiKGWDH8SqOC5p2b3LR3oHIuLiSxc1IUeZf2fBlVHOPTxxj-d8PpHbgsMmfumRTSHFWaa2ZeR-_ByUcjVYO3pBTGxkGso7ArHNOfHQE-Mzk3pE=w2400"
+          width="100%"
+        />
+        <div class="mask">
+          <div class="caption">
+            <p>graphics programming final work</p>
+          </div>
+        </div>
+      </div>
+    </router-link>
+
+    <router-link to="/works/190705">
+      <div class="works">
+        <img
+          src="https://lh3.googleusercontent.com/3Nzk6qaLx8CAcnNVFhZD8xqDK4fvWBP7lg0gDNFeerhFzspV4s0oV_cA4Ogi6hNAWu-mAFutCvr6gMkFlTy7FW6uAwV-f-h19oNTThPJrluJygMSDGI3T1ydxTT2KGfdS3C9DR_IjC8=w2400"
+          width="100%"
+        />
+        <div class="mask">
+          <div class="caption">
+            <p>Computer Music1 group work</p>
+          </div>
+        </div>
+      </div>
+    </router-link>
+
+    <router-link to="/works/190602">
+      <div class="works">
+        <img
+          src="https://lh3.googleusercontent.com/jUAeRx87g41nENrvKCMNhBO0HMcMmDJBUEUD_iI1QWyZHNJe8imc4c8C6WxD76UOASoZqyvFKwRRbUy1IJm_BXWK-AiDzLOovn4gqCaqAmB8lHINJCW-xDvptwBoMahvxpvHV-GfGfU=w2400"
+          width="100%"
+        />
+        <div class="mask">
+          <div class="caption">
+            <p>
+              visual system for
+              <br />SourceCord Vol.3 & SFC X-Music DJ Night
+            </p>
+          </div>
+        </div>
+      </div>
+    </router-link>
+  </body>
+</section>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
