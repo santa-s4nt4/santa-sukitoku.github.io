302ab3754a051039ec9e7f35a8e9308b12ab7a27
santa-sukitoku <santanaruse@gmail.com>
2020-08-16 22:20:05 +0900

    add: 新しいpostを作成

---
+++ pages/works/200816.vue
@@ -0,0 +1,72 @@
+<template>
+<section class="container">
+  <body class="allContent">
+    <div class="captionContent">
+      <p>- LED sync web-site for Save The Metro Compilations -</p>
+    </div>
+    <div class="content">
+      <p>- gallery -</p>
+
+      <img
+        class="magnifiable"
+        src="https://lh3.googleusercontent.com/Ls_ZkG_ZlmgPDlpF3361hX_l6s7HT1Bm9TnMQameswl5-EednMlMeNYoxA67Q_rTKxViKpUYNKer-iivW9Gc5pdUbB21zYLgmsa4QiLM5RezZHDcYbXHdi_zPo82BvBnAUDgj3P5lfE=w2400"
+        itemprop="image"
+        width="100%"
+      />
+      <blockquote class="twitter-tweet">
+        <p lang="ja" dir="ltr">
+          今回、配信twitchみれつつ現場のLEDと背景色をシンクするサイトをひとネタ作りました。メイン構築は
+          <a
+            href="https://twitter.com/santa_sukitoku?ref_src=twsrc%5Etfw"
+          >@santa_sukitoku</a> 、あと
+          <a href="https://twitter.com/nomnommorn?ref_src=twsrc%5Etfw">@nomnommorn</a> もやってくれて、僕もちょっと。部屋を暗くして画面広げて観るとメトロにいる気分になれるハズ…！
+          <br />
+          <br />SaveTheMetroDAY2
+          <a href="https://t.co/Fiwp2ObCow">https://t.co/Fiwp2ObCow</a>
+          <a href="https://t.co/CRC4JUQKMo">pic.twitter.com/CRC4JUQKMo</a>
+        </p>&mdash; Kousei Ikeda (@ikekou)
+        <a
+          href="https://twitter.com/ikekou/status/1276842702290604034?ref_src=twsrc%5Etfw"
+        >June 27, 2020</a>
+      </blockquote>
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+    <div class="item">
+      <p>
+        2020/06/27に
+        <a href="https://www.twitch.tv/metro_kyoto">twitch.tv/metro_kyoto</a>で配信された
+        <a
+          href="https://metro-streaming.com/2020/06/20/save-the-metro-compilations-curated-by-kenichi-itoi/"
+        >METRO 30周年記念配信 -DAY2- Save The Metro Compilations curated by Ken’ichi Itoi</a> での演出に使われたLEDバーの光と背景色が同期する視聴Webサイト．
+      </p>
+    </div>
+    <div class="itemSub">
+      <p>
+        使用技術 : TouchDesigner / Python / JavaScript / Firebase Realtime Database
+        <br />Link :
+        <a
+          href="https://metro-streaming.com/2020/06/20/save-the-metro-compilations-curated-by-kenichi-itoi/"
+          target="_blank"
+        >METRO</a>
+      </p>
+      <div class="members">
+        <p>
+          Members :
+          <br />-
+          <a href="https://santa-sukitoku.netlify.app/">Santa</a> : Main Programmer
+          <br />-
+          <a href="https://twitter.com/nomnommorn">sacco</a> : Web Designer
+          <br />-
+          <a href="https://ikekou.jp/">Kousei Ikeda</a> : Assistant Programmer
+        </p>
+      </div>
+    </div>
+  </body>
+</section>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
