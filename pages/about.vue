ce9048d2db73a03abd5c2a89b0e58d88b45f488c
santa-sukitoku <santanaruse@gmail.com>
2020-10-05 14:10:55 +0900

    add: Performance部分を追記

--- pages/about.vue
+++ pages/about.vue
@@ -1,172 +1,188 @@
 <template>
-<section class="container">
-  <div>
-    <transition name="page">
-      <nuxt />
-    </transition>
-  </div>
-  <body class="allAbout">
-    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet" />
-    <div class="aboutAuthor">
-      <p>Santa is a Japan based Visual Programmer.</p>
-      <div class="aboutLogo">
-        <!-- <img
+  <section class="container">
+    <div>
+      <transition name="page">
+        <nuxt />
+      </transition>
+    </div>
+    <body class="allAbout">
+      <link
+        href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
+        rel="stylesheet"
+      />
+      <div class="aboutAuthor">
+        <p>Santa is a Japan based Visual Programmer.</p>
+        <div class="aboutLogo">
+          <!-- <img
           src="https://lh3.googleusercontent.com/71jjT8idP50_o2ZMNoUDdEAmUgfV2_gMRb32clSqFUGPLgtMIopT-pB4FDhbvuZYh-xLT9fG_cKgcr5GWYIx9piUbKtnjEgbRifenoDzomxQ8y4DmNlw8I155-OUNeAAS2sxRSjFjLA=w2400"
           alt="logo"
           width="300px"
         />-->
-        <img
-          src="https://lh3.googleusercontent.com/uOwasJm4zbD1gp3n5k4lia1sm2czcPx4UPthHpfQy1iwyupi_sbbddOj_1iu4r5ZwlG8Gx0ZB4B_Ep1t_F0wPkx3Uxnpzu_DRnKtEW3I8PwGyWEhmIVwh-atadxAY8ag2kcQaGF9gz8=w2400"
-          alt="profile"
-          width="300px"
-        />
-      </div>
-      <div class="aboutAuthorSub">
-        <p>
-          Visual Programmer / Visual Jockey / Student
-          <br />ビジュアルパフォーマンス，Visual Jockey，インスタレーション作品制作を行う．
-          <br />2000年大阪府生まれ．長野県/大阪府育ち．神奈川県在住．
-          <br />アートユニット
-          <a href>健康推進委員会</a>
-          として活動している．
-        </p>
-        <div class="aboutLink">
-          <a href="https://twitter.com/santa_sukitoku" target="_blank">
-            <i class="fab fa-twitter"></i>
-          </a>
-          <a href="https://github.com/santa-sukitoku" target="_blank">
-            <i class="fab fa-github"></i>
-          </a>
-          <a href="https://soundcloud.com/santa-n" target="_blank">
-            <i class="fab fa-soundcloud"></i>
-          </a>
-          <a
-            href="https://open.spotify.com/user/21i5tt577asz4phxd3rk7xibi?si=tVJRdrfzSrCjTmRyphYqdQ"
-            target="_blank"
-          >
-            <i class="fab fa-spotify"></i>
-          </a>
-          <a href="https://www.instagram.com/santa_sukitoku/" target="_blank">
-            <i class="fab fa-instagram"></i>
-          </a>
+          <img
+            src="https://lh3.googleusercontent.com/uOwasJm4zbD1gp3n5k4lia1sm2czcPx4UPthHpfQy1iwyupi_sbbddOj_1iu4r5ZwlG8Gx0ZB4B_Ep1t_F0wPkx3Uxnpzu_DRnKtEW3I8PwGyWEhmIVwh-atadxAY8ag2kcQaGF9gz8=w2400"
+            alt="profile"
+            width="300px"
+          />
+        </div>
+        <div class="aboutAuthorSub">
+          <p>
+            Visual Programmer / Visual Jockey / Student
+            <br />ビジュアルパフォーマンス，Visual
+            Jockey，インスタレーション作品制作を行う．
+            <br />2000年大阪府生まれ．長野県/大阪府育ち．神奈川県在住．
+          </p>
+          <div class="aboutLink">
+            <a href="https://twitter.com/santa_sukitoku" target="_blank">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="https://github.com/santa-sukitoku" target="_blank">
+              <i class="fab fa-github"></i>
+            </a>
+            <a href="https://soundcloud.com/santa-n" target="_blank">
+              <i class="fab fa-soundcloud"></i>
+            </a>
+            <a
+              href="https://open.spotify.com/user/21i5tt577asz4phxd3rk7xibi?si=tVJRdrfzSrCjTmRyphYqdQ"
+              target="_blank"
+            >
+              <i class="fab fa-spotify"></i>
+            </a>
+            <a href="https://www.instagram.com/santa_sukitoku/" target="_blank">
+              <i class="fab fa-instagram"></i>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="aboutCareer">
-      <p>- Skills -</p>
-      <div class="aboutCareerSub">
-        <p>
-          [Visual Tool] TouchDesigner / GLSL / Processing / p5.js
-          <br />[Sound Tool] Cycling 74 Max / Ableton Live
-          <br />[Programming Language] Python
-          <br />[Frontend] Nuxt.js / HTML / SCSS
-        </p>
+      <div class="aboutCareer">
+        <p>- Skills -</p>
+        <div class="aboutCareerSub">
+          <p>
+            [Visual Tool] TouchDesigner / GLSL / Processing / p5.js
+            <br />[Sound Tool] Cycling 74 Max / Ableton Live <br />[Programming
+            Language] Python <br />[Frontend] Nuxt.js / HTML / SCSS
+          </p>
+        </div>
       </div>
-    </div>
-    <div class="aboutCareer">
-      <p>- Performances -</p>
-      <div class="aboutCareerSub">
-        <p>
-          [2020.06]
-          <a
-            href="https://metro-streaming.com/2020/06/20/save-the-metro-compilations-curated-by-kenichi-itoi/"
-            target="_blank"
-          >METRO 30周年記念配信 -DAY2- Save The Metro Compilations curated by Ken’ichi Itoi</a> - Visual
-          <br />[2020.04]
-          <a
-            href="https://twitter.com/CCLab_SFC/status/1251122493642817538?s=20"
-            target="_blank"
-          >x-Music Online Vol.00</a> - Visual
-          <br />[2020.04]
-          <a
-            href="https://twitter.com/rhizomatiks/status/1245636361731768321?s=20"
-            target="_blank"
-          >Staying Tokyo Vol.00</a> - Visual
-          <br />[2020.02]
-          <a href="http://ochiaisoup.com/?p=3734" target="_blank">Algorave Tokyo</a> - Visual
-          <br />[2019.10]
-          <router-link to="works/191024">SFC Creative Week</router-link>&nbsp;- Visual
-          <br />[2019.07]
-          <a
-            href="https://sukitoku.hatenablog.com/entry/190617"
-            target="_blank"
-          >X-Music DJ Night</a> - Visual
-          <br />[2019.06]
-          <a
-            href="https://kagurane.com/schedules/view/1156"
-            target="_blank"
-          >SourceCord Vol.3</a> - Visual
-          <br />[2018.11]
-          <a
-            href="http://club-stomp.com/2018/11/09/algorave-osaka/"
-            target="_blank"
-          >Algorave Osaka</a> - Visual
-        </p>
+      <div class="aboutCareer">
+        <p>- Performances -</p>
+        <div class="aboutCareerSub">
+          <p>
+            [2020.09]
+            <a
+              href="https://metro-streaming.com/2020/06/20/save-the-metro-compilations-curated-by-kenichi-itoi/"
+              target="_blank"
+              >Playing Tokyo Vol.11</a
+            >
+            - Visual <br />
+            [2020.06]
+            <a
+              href="https://metro-streaming.com/2020/06/20/save-the-metro-compilations-curated-by-kenichi-itoi/"
+              target="_blank"
+              >METRO 30周年記念配信 -DAY2- Save The Metro Compilations curated
+              by Ken’ichi Itoi</a
+            >
+            - Visual <br />[2020.04]
+            <a
+              href="https://twitter.com/CCLab_SFC/status/1251122493642817538?s=20"
+              target="_blank"
+              >x-Music Online Vol.00</a
+            >
+            - Visual <br />[2020.04]
+            <a
+              href="https://twitter.com/rhizomatiks/status/1245636361731768321?s=20"
+              target="_blank"
+              >Staying Tokyo Vol.00</a
+            >
+            - Visual <br />[2020.02]
+            <a href="http://ochiaisoup.com/?p=3734" target="_blank"
+              >Algorave Tokyo</a
+            >
+            - Visual <br />[2019.10]
+            <router-link to="works/191024">SFC Creative Week</router-link
+            >&nbsp;- Visual <br />[2019.07]
+            <a
+              href="https://sukitoku.hatenablog.com/entry/190617"
+              target="_blank"
+              >X-Music DJ Night</a
+            >
+            - Visual <br />[2019.06]
+            <a href="https://kagurane.com/schedules/view/1156" target="_blank"
+              >SourceCord Vol.3</a
+            >
+            - Visual <br />[2018.11]
+            <a
+              href="http://club-stomp.com/2018/11/09/algorave-osaka/"
+              target="_blank"
+              >Algorave Osaka</a
+            >
+            - Visual
+          </p>
+        </div>
       </div>
-    </div>
-    <div class="aboutCareer">
-      <p>- Exhibitions -</p>
-      <div class="aboutCareerSub">
-        <p>coming soon...</p>
+      <div class="aboutCareer">
+        <p>- Exhibitions -</p>
+        <div class="aboutCareerSub">
+          <p>coming soon...</p>
+        </div>
       </div>
-    </div>
-    <div class="aboutCareer">
-      <p>- Education -</p>
-      <div class="aboutCareerSub">
-        <p>
-          [2020.10 - Current]
-          <a
-            href="https://wakita.sfc.keio.ac.jp/index.html"
-            target="_blank"
-          >Wakita Laboratory</a>,
-          at
-          <a href="https://www.keio.ac.jp/" target="_blank">Keio University</a>
-          <a href="https://www.sfc.keio.ac.jp" target="_blank">SFC</a>
-          <br />[2020.04 - Current]
-          <a
-            href="https://x-music.sfc.keio.ac.jp/"
-            target="_blank"
-          >x-Music Laboratory</a>,
-          at
-          <a href="https://www.keio.ac.jp/" target="_blank">Keio University</a>
-          <a href="https://www.sfc.keio.ac.jp" target="_blank">SFC</a>
-          <br />[2019.04 - Current]
-          <a
-            href="https://cclab.sfc.keio.ac.jp"
-            target="_blank"
-          >Computational Creativity Lab</a>,
-          at
-          <a href="https://www.keio.ac.jp/" target="_blank">Keio University</a>
-          <a href="https://www.sfc.keio.ac.jp" target="_blank">SFC</a>
-          <br />[2016.04 - 2019.03]
-          <a
-            href="https://www.seikyo.ed.jp"
-            target="_blank"
-          >Seikyo Gakuen High School</a>
-        </p>
+      <div class="aboutCareer">
+        <p>- Education -</p>
+        <div class="aboutCareerSub">
+          <p>
+            [2020.10 - Current]
+            <a href="https://wakita.sfc.keio.ac.jp/index.html" target="_blank"
+              >Wakita Laboratory</a
+            >, at
+            <a href="https://www.keio.ac.jp/" target="_blank"
+              >Keio University</a
+            >
+            <a href="https://www.sfc.keio.ac.jp" target="_blank">SFC</a>
+            <br />[2020.04 - Current]
+            <a href="https://x-music.sfc.keio.ac.jp/" target="_blank"
+              >x-Music Laboratory</a
+            >, at
+            <a href="https://www.keio.ac.jp/" target="_blank"
+              >Keio University</a
+            >
+            <a href="https://www.sfc.keio.ac.jp" target="_blank">SFC</a>
+            <br />[2019.04 - Current]
+            <a href="https://cclab.sfc.keio.ac.jp" target="_blank"
+              >Computational Creativity Lab</a
+            >, at
+            <a href="https://www.keio.ac.jp/" target="_blank"
+              >Keio University</a
+            >
+            <a href="https://www.sfc.keio.ac.jp" target="_blank">SFC</a>
+            <br />[2016.04 - 2019.03]
+            <a href="https://www.seikyo.ed.jp" target="_blank"
+              >Seikyo Gakuen High School</a
+            >
+          </p>
+        </div>
       </div>
-    </div>
-    <div class="aboutCareer">
-      <p>- Work Experiences -</p>
-      <div class="aboutCareerSub">
-        <p>
-          [2019.06 - 2019.10]
-          <a href="https://nepps.jp" target="_blank">Nepps</a> Mentor
-          <br />[2019.01 - 2019.03]
-          <a href="https://www.1-10.com" target="_blank">1→10, Inc.</a>
-          internship
-          <br />[2018.03]
-          <a href="https://tech.mercari.com/entry/2017/12/20/170000" target="_blank">
-            Mercari
-            BOLD Scholarship for SXSW 2018
-          </a>
-          <br />
-          <br />
-        </p>
+      <div class="aboutCareer">
+        <p>- Work Experiences -</p>
+        <div class="aboutCareerSub">
+          <p>
+            [2019.06 - 2019.10]
+            <a href="https://nepps.jp" target="_blank">Nepps</a> Mentor
+            <br />[2019.01 - 2019.03]
+            <a href="https://www.1-10.com" target="_blank">1→10, Inc.</a>
+            internship
+            <br />[2018.03]
+            <a
+              href="https://tech.mercari.com/entry/2017/12/20/170000"
+              target="_blank"
+            >
+              Mercari BOLD Scholarship for SXSW 2018
+            </a>
+            <br />
+            <br />
+          </p>
+        </div>
       </div>
-    </div>
-  </body>
-</section>
+    </body>
+  </section>
 </template>
 
 <script>
