eb6737a81c8f1c448806fe1c18eda429c627f23c
santa-sukitoku <santanaruse@gmail.com>
2020-11-17 22:40:33 +0900

    fix: サイトの名前を変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || 'santa-sukitoku',
+    title: process.env.npm_package_name || 'santa-s4nt4',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
