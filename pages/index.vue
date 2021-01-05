0f03b4d791a23294acef01876e84890e0447baea
santa-sukitoku <santanaruse@gmail.com>
2021-01-06 04:03:20 +0900

    fix: indexページに-14を追加

--- pages/index.vue
+++ pages/index.vue
@@ -48,6 +48,20 @@
         </div>
       </router-link>
 
+      <router-link to="/works/200718">
+        <div class="works">
+          <img
+            src="https://lh3.googleusercontent.com/PMH-SlwKA70K1SXu5FTDolkKzmGps-EcTueBhsx76H-RgHL7oYw73tejsxE3iQPv6WjjkCefazmv10TD3OG_eEKGt8vqXg7RWkD-9-J4GtY5ke9oqN9ESWXHA3_P-tZBPk5JUjZ_zV4=w2400"
+            width="100%"
+          />
+          <div class="mask">
+            <div class="caption">
+              <p>-14&#x2103;</p>
+            </div>
+          </div>
+        </div>
+      </router-link>
+
       <router-link to="/works/200627">
         <div class="works">
           <img
